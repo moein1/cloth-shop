35888e13b4f39d276fe31f36bc9d0f36654bb4b6
mohammad karimi <m35_karimi@yahoo.com>
2018-01-27 09:11:36 +1000

    Adding redux devtools for accessing to redux in the chrome tab

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore ,compose } from 'redux';
 
 import './styles/css/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import reducer from './store/reducer';
 
-const store = createStore(reducer);
+const store = createStore(reducer ,
+     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() || compose );
 
 const app = (
     <Provider store =  {store} >
