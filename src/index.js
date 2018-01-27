a672fbb1f48253f6c9b07a5a876e395cfa8f550b
mohammad karimi <m35_karimi@yahoo.com>
2018-01-27 14:09:59 +1000

    setuping the thunk middleware for asyncronous

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
-import {Provider} from 'react-redux';
-import {createStore ,compose } from 'redux';
+import {Provider } from 'react-redux';
+import {createStore ,compose ,applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import './styles/css/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import reducer from './store/reducers/clothBuilder';
 
-const store = createStore(reducer ,
-     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() || compose );
+const copmoseEnhancers = 
+//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() || 
+compose;
+
+const store = createStore(reducer ,copmoseEnhancers(applyMiddleware(thunk)));
 
 const app = (
     <Provider store =  {store} >
