18a92280020812b39c17387ddbe764342cb5359c
mohammad karimi <m35_karimi@yahoo.com>
2018-01-28 18:36:34 +1000

    Refactoring the purchase reducer for having leaner code in the switch statement.

--- src/store/reducers/purchase.js
+++ src/store/reducers/purchase.js
@@ -8,31 +8,40 @@ const initialState = {
     purchased: false
 }
 
+const purchaseSuccess = (state ,action) =>{
+    const newOrder = {
+        ...action.orderData,
+        id: action.orderId
+    }
+    return {
+        ...state,
+        error: null,
+        loading: false,
+        orders: state
+            .orders
+            .concat(newOrder),
+        purchased: true
+    }
+}
+
+const purchaseFail = (state,action) =>{
+    return {
+        ...state,
+        error: action.error,
+        loading: false
+    }
+}
+
+
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionType.PURCHASE_CLOTH_START:
             return updatedObject(state, {loading: true});
         case actionType.PURCHASE_CLOTH_SUCCESS:
-            const newOrder = {
-                ...action.orderData,
-                id: action.orderId
-            }
-            return {
-                ...state,
-                error: null,
-                loading: false,
-                orders: state
-                    .orders
-                    .concat(newOrder),
-                purchased: true
-
-            }
+            return purchaseSuccess(state,action);
         case actionType.PURCHASE_CLOTH_FAIL:
-            return {
-                ...state,
-                error: action.error,
-                loading: false
-            }
+            return purchaseFail(state , action);
         case actionType.PURCHASE_INIT:
             return updatedObject(state, {purchased: false});
         default:
