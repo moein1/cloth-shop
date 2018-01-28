cd1a701972abc4bb10e3d2ae5e376de7164f45d1
mohammad karimi <m35_karimi@yahoo.com>
2018-01-28 18:54:27 +1000

    continue refactoring the order redunder

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,34 +1,37 @@
 import actionTypes from '../actions/actionTypes';
-import {updatedObject} from '../utility';  
-
+import {updatedObject} from '../utility';
 
 const initialState = {
     orders: [],
     error: false,
-    loading : false
+    loading: false
 }
 
-const setOrders = (state , action)=>{
+const setOrders = (state, action) => {
     return {
         ...state,
         orders: action.orders,
         error: false,
-        loading : false
+        loading: false
+    }
+}
+
+const setOrderFail = (state, action) => {
+    return {
+        ...state,
+        error: true,
+        loading: false
     }
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.SET_ORDERS_START:
-            return updatedObject(state , {loading : true} )
+            return updatedObject(state, {loading: true})
         case actionTypes.SET_ORDERS:
-            return setOrders(state , action);
+            return setOrders(state, action);
         case actionTypes.SET_ORDERS_FAIL:
-            return {
-                ...state,
-                error: true,
-                loading : false
-            }
+            return setOrderFail(state, action);
         default:
             return state;
     }
