415329e62e7362c1d12ee59c5ee41cd1d6cabb25
mohammad karimi <m35_karimi@yahoo.com>
2018-01-29 10:25:41 +1000

    Delete some navigation items that we do not need them at this stage

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,10 +4,7 @@ import NavigationItem from './NavigationItem/NavigationItem';
 const navigationItem = (props) => (
     <ul>
         <NavigationItem exact link="/">Home</NavigationItem>
-        <NavigationItem link="#">About</NavigationItem>
-        <NavigationItem link="/checkout">Checkout</NavigationItem>
         <NavigationItem link="/orders">Orders</NavigationItem>
-        <NavigationItem link="#">Summary</NavigationItem>
     </ul>
 )
 
