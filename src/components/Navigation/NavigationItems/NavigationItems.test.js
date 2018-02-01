106629e25a4b2f4ada90cace6070937310fe1551
mohammad karimi <m35_karimi@yahoo.com>
2018-02-01 18:21:37 +1000

    Add another unit test for NavigationItems component and use the contains method for testing to have the NavigationItem for logout when we authenticated.

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -9,8 +9,23 @@ import NavigationItem from './NavigationItem/NavigationItem';
 configure({adapter: new Adpater()})
 
 describe('<NavigationItems/>', () => {
-    it('it should render two <NavigationItem /> if not authenticated', () => {
-        const wrapper = shallow(<NavigationItems />);
-        expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    let wrapper = null;
+    beforeEach(()=>{
+        wrapper = shallow(<NavigationItems />);
     })
+
+    it('it should render two <NavigationItem /> if not authenticated', () => {        
+        expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    });
+
+    it('it should render three <NavigationItem /> if authenticated' , ()=>{
+       // const wrapper = shallow(<NavigationItems auth />);
+       wrapper.setProps({auth : true});
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });
+
+    it('it should contains the logout item if authenticated' , ()=>{
+        wrapper.setProps({auth : true});
+        expect(wrapper.contains(<NavigationItem link="/logout" >Logout</NavigationItem>)).toEqual(true);
+    } )
 })
